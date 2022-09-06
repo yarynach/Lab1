ebb8a4cb368540eb791018e477160c0b940c0f1b
Yaryna Chernysh <yaryna.chernish18@gmail.com>
2022-09-07 00:45:47 +0300

    Added all method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import lab1.LucasNumber;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        LucasNumber num = new LucasNumber(4);
-        System.out.println(num.getNumber());
-        System.out.print(num.G);
+        System.out.println("Enter which Lucas number you want to get:");
+        Scanner scan = new Scanner(System.in);
+        int nl = (scan.nextInt())-1;
+        LucasNumber num = new LucasNumber(nl);
+        //System.out.println();
+        System.out.println(num.getNumber(nl)+" is number "+num.getOrder());
+        System.out.println("The statements n*L(n+1)>(n+1)*L(n)) is " + num.check());
+        System.out.println(num.getOrder()+"*"+ num.getNumber(nl+1)+">"+ (num.getOrder()+1)+"*"+num.getNumber(nl));
     }
 }
--- src/lab1/LucasNumber.java
+++ src/lab1/LucasNumber.java
@@ -1,15 +1,20 @@
 package lab1;
 
 public class LucasNumber {
-    int n;
+    private int n;
+    int number;
     public static final double G = ((1+Math.sqrt(5))/2);
     public LucasNumber( int n){
         this.n=n;
     }
-    public  double getNumber(){
-        double finalnum;
-        n=n+1;
-        finalnum = Math.pow(G,n)+(Math.pow(-G,-n));
-        return finalnum;
+    public  int getNumber(int num){
+        number = (int)(Math.pow(G,num)+(Math.pow(-G,-num)));
+        return number;
+    }
+    public boolean check(){
+        return (n*getNumber(n+1)>(n+1)*getNumber(n));
+    }
+    public int getOrder(){
+        return (n+1);
     }
 }
