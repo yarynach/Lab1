3bafc4c56ad8c1878bb187f3049a2079494996bf
Yaryna Chernysh <yaryna.chernish18@gmail.com>
2022-09-09 15:40:55 +0300

    first commit

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -19,7 +19,6 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int nl = (scan.nextInt())-1;
         LucasNumber num = new LucasNumber(nl);
-        //System.out.println();
         System.out.println(num.getNumber(nl)+" is number "+num.getOrder());
         System.out.println("The statements n*L(n+1)>(n+1)*L(n)) is " + num.check());
         System.out.println(num.getOrder()+"*"+ num.getNumber(nl+1)+">"+ (num.getOrder()+1)+"*"+num.getNumber(nl));
